554ad97c780d0d26604057f21bb8f72620e3891f
macong2023 <147220656+macong2023@users.noreply.github.com>
2024-04-08 16:22:42 +0800

    MyFormCard, MyFormCardET, MyFormET, MyModalET (#4)

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,8 @@
 import MyModal from './components/MyModal/MyModal';
 import MyStandardTableTwo from './components/MyStandardTableTwo/MyStandardTableTwo';
+import MyFormCard from './components/MyFormCard/index';
+import MyFormCardET from './components/MyFormCardET';
+import MyFormET from './components/MyFormET';
+import MyModalET from "./components/MyModalET";
 
-export { MyModal,MyStandardTableTwo }
+export { MyModal, MyStandardTableTwo, MyFormCard, MyFormCardET, MyFormET, MyModalET }
